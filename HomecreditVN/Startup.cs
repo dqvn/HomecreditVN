de9efd299203e2376115d0329066bbb2e36fe3ad
dqvn <dqvn2002@gmail.com>
2016-10-31 17:38:02 +0700

    Commit message

---
+++ HomecreditVN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomecreditVN.Startup))]
+namespace HomecreditVN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
